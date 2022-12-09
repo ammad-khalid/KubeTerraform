b971352e0a4075e69c37b6ba790c25618c5a3bb7
ammad-khalid <55195052+ammad-khalid@users.noreply.github.com>
2022-12-09 22:23:43 +0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -7,21 +7,10 @@ resource "aws_eks_cluster" "dev-eks" {
     /*cluster_security_group_id = "${aws_security_group.kubernetes.id}"*/
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
     aws_iam_role_policy_attachment.AmazonEKSServicePolicy
   ]
 }
-
-
-
-/*output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
-}*/
